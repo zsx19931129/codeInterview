22d77d7171f6933bfd8a1dffac826e16e088d36b
Shawn Zhang <zsx19931129@163.com>
2016-04-11 16:20:52 +0800

    二叉树的镜像

---
+++ MirrorOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+    	if(pRoot == NULL){
+    		return ;
+    	}
+    	if(pRoot->left == NULL && pRoot->right == NULL){
+    		return ;
+    	}
+
+    	TreeNode* tmp = pRoot->left;
+    	pRoot->left = pRoot->right;
+    	pRoot->right = tmp;
+
+    	if(pRoot->left){
+    		Mirror(pRoot->left);
+    	}
+    	if(pRoot->right){
+    		Mirror(pRoot->right);
+    	}
+    }
+};
