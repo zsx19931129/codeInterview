4639d703740ae25b4524d9aa13250088f18bcdd1
Shawn Zhang <zsx19931129@163.com>
2016-12-21 01:12:36 +0800

    get fb user email address within two hops, bfs

---
+++ Zillow/EmailAddressWithinTwoHop.py
@@ -0,0 +1,28 @@
+class FacebookUser(object):
+	def __init__(self, uid, firstname, lastname, email):
+		self.uid = uid
+		self.firstname = firstname
+		self.lastname = lastname
+		self.email = email
+
+class GraphNode(object):
+	def __init__(self, user):
+		self.user = user
+		self.neighbors = list()
+
+def get_email_addr_within_two_hop(node):
+	ret = list()
+	visited, q = set(), [node]
+	hops = 2
+	while q:
+		cur_node = q.pop(0)
+		visited.add(cur_node.user.uid)
+		for neighbor in cur_node.neighbors:
+			if neighbor not in visited:
+				if neighbor.user.lastname == cur_node.user.lastname:
+					result.append(neighbor.user.email)
+				q.add(neighbor)
+		hops -= 1
+		if hops==0:
+			break
+	return ret
