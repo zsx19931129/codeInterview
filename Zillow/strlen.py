05055c833de22a3f12bdc420ea9c4f8a3433fcd3
Shawn Zhang <zsx19931129@163.com>
2016-12-21 00:50:10 +0800

    strlen recursive

---
+++ Zillow/strlen.py
@@ -0,0 +1,10 @@
+def strlen(s, idx):
+	try:
+		s[idx]
+		return 1 + strlen(s, idx+1)
+	except IndexError:
+		return 0
+
+if __name__ == '__main__':
+    s = 'best wishes!'
+    assert strlen(s, 0) == len(s)
